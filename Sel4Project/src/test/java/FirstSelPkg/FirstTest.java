d776db0a038f14b5b86bacb3dd67cb0e8e02517e
Arnav <Arnav@ArnavPC>
2024-01-28 23:30:38 +0530

    first Commit By San - Jan 28, 2024

---
+++ Sel4Project/src/test/java/FirstSelPkg/FirstTest.java
@@ -0,0 +1,29 @@
+package FirstSelPkg;
+
+import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+	
+		//System.setProperty("webdriver.chrome.driver","C:\\BuluHome\\Sanju\\Work\\SW\\CromeDriver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		 
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		driver.quit();
+		
+	}
+}
